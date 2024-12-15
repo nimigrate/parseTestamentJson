64402973d4695e808c7fafff8534740832fbe315
litlighilit <litlighilit@foxmail.com>
2024-12-15 13:23:31 +0800

    fix(typesMeth): RunRecode.filepath now works on Windows

--- src/parseTestamentJson/typesMeth.nim
+++ src/parseTestamentJson/typesMeth.nim
@@ -1,9 +1,14 @@
 
 import ./types
 import std/json
-from std/strutils import removePrefix
+from std/strutils import removePrefix, replace
 const NodeNameKey* = "name"
+
+import std/os
+const FilePathSep* = '/'
 
 func filepath*(self: RunRecord): string =
   result = self.node[NodeNameKey].getStr()
+  when PathSep != FilePathSep:
+    result = result.replace(PathSep, FilePathSep)
   result.removePrefix"tests/"
